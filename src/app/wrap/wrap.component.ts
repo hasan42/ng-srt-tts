8d8eaea8c0138c800b2204932e175040ab70ab41
rpokosov <hasan83842@gmail.com>
2019-11-27 14:44:20 +0300

    make read

--- src/app/wrap/wrap.component.ts
+++ src/app/wrap/wrap.component.ts
@@ -22,6 +22,10 @@ export class WrapComponent implements OnInit {
     pitch: 1
   };
 
+  textArr: any = [];
+
+  current: number = 0;
+
   constructor() {
     if ('speechSynthesis' in window) {
       this.msg = 'Support';
@@ -46,10 +50,13 @@ export class WrapComponent implements OnInit {
     this.form.voice = voice
   }
 
-  speak() {
+  speak(nowmsg) {
+    this.current = nowmsg;
     let msg = new SpeechSynthesisUtterance();
+
+    let delay = this.textArr[this.current].time.delay;
   
-    msg.text = this.form.text;
+    msg.text = this.textArr[this.current].text;
     msg.volume = this.form.volume;
     msg.rate = this.form.rate;
     msg.pitch = this.form.pitch;
@@ -57,10 +64,56 @@ export class WrapComponent implements OnInit {
     if (this.form.voice) {
       msg.voice = speechSynthesis.getVoices().filter(voice => voice.name == this.form.voice)[0];
     }
+    setTimeout(()=>{
+      window.speechSynthesis.speak(msg);
+      this.speak(this.current + 1)
+    }, delay)
     
-    window.speechSynthesis.speak(msg);
   }
 
+  textIt() {
+    this.textArr = this.form.text.split('\n\n');
+
+    let newArr = [];
+    let delayCommon = 0;
+    this.textArr.forEach((item)=>{
+      let obj = {
+        id: null,
+        time: {
+          start: null,
+          end: null,
+          delay: null,
+          timer: null
+        }, 
+        text: null
+      };
+      let arr = item.split('\n');
+
+      [obj.id, , ...obj.text] = arr;
+
+      obj.text = obj.text.join(' ');
+
+      let timeArr = arr[1].split(' --> ');
+      [obj.time.start, obj.time.end] = timeArr;
+
+      let delayArr = obj.time.start.split(':');
+      let delayHH = ((Number(delayArr[0]) * 60) * 60) * 1000
+      let delayMM = (Number(delayArr[1]) * 60) * 1000
+      let delaySS = Number(delayArr[2].replace(',', '.')) * 1000
+      console.log(delayArr, delayHH, delayMM, delaySS);
+      obj.time.timer = delayHH + delayMM + delaySS
+      obj.time.delay = obj.time.timer - delayCommon
+      delayCommon = obj.time.timer
+
+      newArr.push(obj);
+    });
+    this.textArr = newArr;
+    console.log(newArr);
+  }
+
+  play() {
+    this.speak(0)
+  }
   pause() {
     this.paused = !this.paused;
     window.speechSynthesis.pause();
